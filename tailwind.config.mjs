03c5c3f50df782a293093a9335998c04c7e5e3e7
DevVali <90271108+DevVali@users.noreply.github.com>
2023-12-27 15:44:57 +0100

    initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	theme: {
+		extend: {
+			colors: {
+				text: "#e9dddd",
+				background: "#110e0e",
+				primary: "#91c587",
+				secondary: "#293c25",
+				accent: "#e8e9dd",
+			},
+		},
+	},
+	plugins: [],
+};
