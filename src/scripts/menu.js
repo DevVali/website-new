8c9a146bde6fbe937416babb94f4353d804c243d
DevVali <90271108+DevVali@users.noreply.github.com>
2023-12-28 07:17:49 +0100

    feat: add navbar

---
+++ src/scripts/menu.js
@@ -0,0 +1,7 @@
+document.querySelector(".hamburger").addEventListener("click", () => {
+	document.querySelector(".nav-links").classList.remove("hidden");
+});
+
+document.querySelector(".hamburger-close").addEventListener("click", () => {
+	document.querySelector(".nav-links").classList.toggle("hidden");
+});
