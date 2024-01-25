3378e3615843c4498884ade2cf218024bb3e26ca
DevVali <90271108+DevVali@users.noreply.github.com>
2024-01-25 18:50:37 +0100

    refactor(components): move navbar pages to config

---
+++ src/config.ts
@@ -0,0 +1,22 @@
+export const navPages = [
+	{
+		url: "",
+		title: "Home",
+		icon: "mdi:home",
+	},
+	{
+		url: "about",
+		title: "About",
+		icon: "mdi:about",
+	},
+	{
+		url: "projects",
+		title: "Projects",
+		icon: "grommet-icons:projects",
+	},
+	{
+		url: "contact",
+		title: "Contact",
+		icon: "mdi:contact",
+	},
+];
