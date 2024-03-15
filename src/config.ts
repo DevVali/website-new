c63beeeb16e38e70c98fd45de0fa3b0834c52fae
DevVali <90271108+DevVali@users.noreply.github.com>
2024-03-15 14:43:30 +0000

    feat: add scrolling to header

--- src/config.ts
+++ src/config.ts
@@ -1,21 +1,17 @@
 export const navPages = [
 	{
-		url: "#",
 		title: "Home",
 		icon: "mdi:home",
 	},
 	{
-		url: "#",
 		title: "About",
 		icon: "mdi:about",
 	},
 	{
-		url: "#",
 		title: "Projects",
 		icon: "grommet-icons:projects",
 	},
 	{
-		url: "#",
 		title: "Contact",
 		icon: "mdi:contact",
 	},
