21aa0550b04d165d22cad45462aa1715d1eda4e8
DevVali <90271108+DevVali@users.noreply.github.com>
2024-04-21 09:44:34 +0000

    build: new idx build script

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,23 +1,33 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
 { pkgs, ... }: {
-  channel = "stable-23.11";
-  packages = [ pkgs.nodejs ];
-  idx.extensions = [ "astro-build.astro-vscode" ];
-  idx.workspace.onCreate.install = ''
-    npm install
-    yes | npx astro add tailwind'';
-  idx.previews = {
-    enable = true;
-    previews = [
-      {
+  # Which nixpkgs channel to use.
+  channel = "stable-23.11"; # or "unstable"
+  # Use https://search.nixos.org/packages to find packages
+  packages = [ pkgs.nodejs_20 ];
+  # Sets environment variables in the workspace
+  env = { };
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [ "astro-build.astro-vscode" ];
+    workspace = {
+      # Runs when a workspace is first created with this `dev.nix` file
+      onCreate = {
+        install = ''
+          npm install
+          yes | npx astro add tailwind'';
+      };
+      # To run something each time the environment is rebuilt, use the `onStart` hook
+    };
+    # Enable previews and customize configuration
+    previews = {
+      enable = true;
+      previews = [{
         command =
           [ "npm" "run" "dev" "--" "--port" "$PORT" "--hostname" "0.0.0.0" ];
         manager = "web";
         id = "web";
-      }
-      {
-        manager = "ios";
-        id = "ios";
-      }
-    ];
+      }];
+    };
   };
 }
